8a14e6e8ebef67f032dd92ff304e030da8c0dbba
Thomas HUET <git@thomash.fr>
2015-02-23 13:02:22 +0100

    Remove tests

--- crawler/pgtest.ml
+++
@@ -1,8 +0,0 @@
-module PG = PGOCaml_generic.Make(Thread)
-
-let main =
-  lwt db = PG.connect ~database:"pantor" ~unix_domain_socket_dir:"/var/run/postgresql" () in
-  lwt () = Lwt_io.printf "ready\n" in
-  PG.close db
-
-let () = Lwt_main.run main
